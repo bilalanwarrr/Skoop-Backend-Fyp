34497d1f0a2419ab183cdcf45d66b202b97899f3
bilalanwarrr <bilalanwer2001@gmail.com>
2023-11-27 22:56:27 +0500

    second commit

--- models/stock_products.js
+++ models/stock_products.js
@@ -4,12 +4,10 @@ var Schema = mongoose.Schema;
 var stockProduct = new Schema({
 	name: { type: String, defualt: '' },
 	price: { type: Number },
-	description: { type: String, defualt: '' },
 	stockProduct_category: {
 		type: mongoose.Types.ObjectId,
 		ref: 'stockCategory',
 	},
-	image: { type: String },
     quantity: {type: Number, default: 0},
 	vendor: { type: mongoose.Types.ObjectId, ref: 'Vendor' },
 });
