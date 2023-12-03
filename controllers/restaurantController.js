b65d079264270f5e3f1ac4025cf82f0e6f8e80fa
bilalanwarrr <bilalanwer2001@gmail.com>
2023-12-03 21:49:37 +0500

    vendor-res routes

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -16,6 +16,8 @@ var Admin = require('../models/admin');
 var DeliveryAddress = require('../models/delivery_addresses');
 var Vendor = require('../models/vendor');
 var StockProduct = require('../models/stock_products');
+var StockCategory = require('../models/stock_categories');
+
 
 exports.register = async (req, res, next) => {
 	var exists = await Restaurant.findOne({ email: req.body.email });
@@ -510,4 +512,25 @@ exports.getAllProducts = asyncHandler(async (req, res, next) => {
 	const items = await StockProduct.find({
 	})
 	res.status(200).json(items)
+})
+
+exports.getSingleVendor = asyncHandler(async (req, res, next) => {
+	const vendor = await Vendor.findById(req.params.id)
+
+	if (!vendor) {
+		return res.status(404).json({ error: 'Vendor not found' });
+	  }
+  
+	res.status(200).json(vendor)
+})
+
+exports.getSingleCategory = asyncHandler(async (req, res, next) => {
+	console.log("Hello IUHVSIUAHVC")
+	const category = await StockCategory.findById(req.params.id)
+
+	if (!category) {
+		return res.status(404).json({ error: 'Category not found' });
+	  }
+  
+	res.status(200).json(category)
 })
